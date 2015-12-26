78b6cb64889d5e5d5e2f29743c46e5a4d06eda78
Alex Telek <imex94@gmail.com>
2015-12-26 21:48:06 +0100

    initialise project

---
+++ SwiftCompiler/SwiftCompiler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftCompiler
+//
+//  Created by Alex Telek on 26/12/2015.
+//  Copyright © 2015 Alex Telek. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
